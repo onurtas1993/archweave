85c7e7f40a2d47e14c4f03e926a839bf69e3922d
Onur <onurtas1993@gmail.com>
2026-07-23 10:15:31 +0200

    Initial Commit

---
+++ src/main/java/com/onurtas/archweave/ArchweaveApplication.java
@@ -0,0 +1,13 @@
+package com.onurtas.archweave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchweaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchweaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurtas/archweave/ArchweaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurtas.archweave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchweaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
